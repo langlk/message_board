3a04c9af828ae10a3ebe3cac6c390b8fd5a09046
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 13:22:29 -0800

    Update message to include username.

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,11 @@ class Message < ActiveRecord::Base
 
   validates_presence_of :user, :group, :content
 
+  before_create :add_username
+
   scope :timeframe, -> (start_time, end_time) { where('created_at >= ? AND created_at < ?', start_time, end_time) }
+
+  def add_username
+    self.username = user.name
+  end
 end
