676a73f6ff2e2436820d43cdaad84b58e59d9c01
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 14:00:29 -0800

    Add base controller back.

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::BaseController < ApplicationController
+  include Response
+  include ResponseErrors
+  
+  attr_reader :current_user
+  before_action :restrict_access
+
+  private
+  def restrict_access
+    api_key = ApiKey.find_by_access_token(params[:key])
+    render json: { error: 'Not Authorized' }, status: :unauthorized unless api_key
+  end
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Not Authorized' }, status: :unauthorized unless @current_user
+  end
+end
