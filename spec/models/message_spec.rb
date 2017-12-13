eb48c17bb5be11763ee7f434abd503d416cd140a
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 15:28:28 -0800

    Add test for timeframe scope.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -18,4 +18,14 @@ describe Message do
       expect(Message.chronological).to eq([message1, message2, message3, message4])
     end
   end
+
+  describe '.timeframe' do
+    it "returns messages created within the timeframe given" do
+      message1 = FactoryBot.create(:message)
+      message2 = FactoryBot.create(:message, user: message1.user, group: message1.group)
+      message3 = FactoryBot.create(:message, user: message1.user, group: message1.group)
+      message2.update(created_at: DateTime.now - 6.days)
+      expect(Message.timeframe(DateTime.now - 1, DateTime.now)).to eq([message1, message3])
+    end
+  end
 end
