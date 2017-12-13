346fdbb9e9b7ca95f49019cb7784beda373e25ae
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 15:17:26 -0800

    Add test for chronological scope.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -7,4 +7,15 @@ describe Message do
   it { should validate_presence_of :user }
   it { should validate_presence_of :group }
   it { should validate_presence_of :content }
+
+  describe '.chronological' do
+    it "returns messages from first to last created" do
+      message1 = FactoryBot.create(:message)
+      message2 = FactoryBot.create(:message, user: message1.user, group: message1.group)
+      message3 = FactoryBot.create(:message, user: message1.user, group: message1.group)
+      message4 = FactoryBot.create(:message, user: message1.user, group: message1.group)
+      message4.update(content: "War Never Changes")
+      expect(Message.chronological).to eq([message1, message2, message3, message4])
+    end
+  end
 end
